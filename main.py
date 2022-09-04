947e7fc7f622a177d44b6426c52c7d9c7d1ff166
Sloop <66267333+spallis93@users.noreply.github.com>
2022-09-04 13:34:45 -0400

    update

--- main.py
+++ main.py
@@ -7,8 +7,17 @@ print("Bot started...")
 def start_command(update, context):
     update.message.reply_text('Type something random to get started')
 
-def help_command(update, context):
-    update.message.reply_text('Ask on Google')
+def convertc_command(update, context):
+    user_input = update.message.text
+    celsius = user_input.split('/convertc ')[1]
+    fahrenheit = ((float(celsius) * 9)/5) + 32
+    update.message.reply_text(str(fahrenheit)+'F')
+
+def convertf_command(update, context):
+    user_input = update.message.text
+    fahrenheit = user_input.split('/convertf ')[1]
+    celsius = ((float(fahrenheit) - 32) * 5 / 9)
+    update.message.reply_text(str(celsius)+'C')
 
 def handle_message(update, context):
     text = str(update.message.text).lower()
@@ -24,7 +33,8 @@ def main():
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start_command))
-    dp.add_handler(CommandHandler("help", help_command))
+    dp.add_handler(CommandHandler("convertc", convertc_command))
+    dp.add_handler(CommandHandler("convertf", convertf_command))
 
     dp.add_handler(MessageHandler(Filters.text, handle_message))
 
@@ -32,6 +42,5 @@ def main():
 
     updater.start_polling()
     updater.idle()
-
 
 main()
--- responses.py
+++ responses.py
@@ -1,5 +1,3 @@
-from datetime import datetime 
-
 def sample_responses(input_text):
     user_message = str(input_text).lower()
 
